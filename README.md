246b5ee45c21fa1d78ac2de8649d6a075f8823a3
danishkhaann <danishkhanog@gmail.com>
2022-07-26 15:58:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mini Blog WebApp
+
+This is a React.JS app
+
+A blogging WebApp created using React.JS
+
+To run the website correctly:
+
+1- In the project directory run: npm intsall (to install node modules)
+
+2- run: npx json-server --watch data/db.json --port 8000   (to run a json-server watch over the database on port 8000)
+
+3- In a new command line run: npm start    (To start the app, the app will run automaticly on port 3000)
+
+
+Thank you!
---
+++ src/Create.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Danish');
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      // history.go(-1);
+      history.push('/');
+    })
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Danish">Danish</option>
+          <option value="Danish-khan">Danish-khan</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
+}
+
+export default Create;
